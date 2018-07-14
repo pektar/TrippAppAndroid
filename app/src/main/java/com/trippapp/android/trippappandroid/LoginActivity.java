b9b394ea68b3baf412d237244d7d8f5d769a43ec
mamad <m.farrokh.1996@gmail.com>
2018-07-14 22:04:49 +0430

    login_android

--- app/src/main/java/com/trippapp/android/trippappandroid/LoginActivity.java
+++ app/src/main/java/com/trippapp/android/trippappandroid/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.trippapp.android.trippappandroid;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.trippapp.Account.AccountServiceGrpc;
+import io.grpc.trippapp.Account.LoginReq;
+import io.grpc.trippapp.Account.LoginResp;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,4 +39,30 @@ public class LoginActivity extends AppCompatActivity {
     public void GotoSingUp(View view) {
         startActivity(new Intent(this, SignUpActivity.class));
     }
+
+    private class LoginGrpc extends AsyncTask<String, String, LoginResp> {
+        private ManagedChannel channel;
+
+        @Override
+        protected LoginResp doInBackground(String... params) {
+            String username_email = params[0];
+            String password = params[1];
+            channel = ManagedChannelBuilder.forAddress("localhost",8585).usePlaintext().build();
+            AccountServiceGrpc.AccountServiceBlockingStub stub = AccountServiceGrpc.newBlockingStub(channel);
+            LoginReq request = LoginReq.newBuilder().setUsernameEmail(username_email).setPassword(password).build();
+            LoginResp response = stub.login(request);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(LoginResp response) {
+            Toast.makeText(LoginActivity.this, "server token : " + response.getToken() + "\n server user "
+                    + response.getUser() + "server message : " +response.getResult().getMessage() +
+                    "\n server succes: " + response.getResult().getSuccuss() , Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
 }
